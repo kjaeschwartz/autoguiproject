e6f581eea1567d2e37fd1e5a4d3fd413fbf119ed
kschwartz <kjaeschwartz@gmail.com>
2021-07-13 15:43:51 -0700

    message

---
+++ pyautogui_image_find.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import pyautogui
+
+from pathlib import Path
+#IMPORTANT!!!!!: SEARCH SCREEN MUST BE IN SPLIT SCREEN MOVE, NOT FULL SCREEN.
+#IMPORTANT!!!!!!!! SEARCH SCREEN MUST BE IN SAME SCREEN AS PYCHARM, BOTH MUST BE ON THE LEFT SCREEN
+wh = pyautogui.size()
+
+
+state_py3 = Path("C:\\Users\kschwartz\Desktop\extra_stuff_i_guess\state-py3")
+
+def check_screenshot_acc(imagefinder,newpngname):
+    print(imagefinder)
+    pyautogui.moveTo(imagefinder,duration = .5)
+    screenshot = pyautogui.screenshot(region=(imagefinder))
+    screenshot.save(state_py3/newpngname)
+
+
+def find_picture_location(png_name):
+    finder_pngs = Path("C:\\Users\kschwartz\Desktop\extra_stuff_i_guess\state-py3\Finder_pngs")
+    openedImage = Image.open(finder_pngs/png_name)
+    imagefinder = pyautogui.locateOnScreen(openedImage,confidence = .93)
+    newpngname = "acctest_results\\acctest_" + png_name
+    check_screenshot_acc(imagefinder,newpngname)
+
+
+#if i wanna be picky i could put this in the function but eh, right now it makes more sense to leave it here
+pngs_to_get = ["DOB_pic.png", "county_png.png","last_pic.png",  "first_pic.png",  "middle_png.png",
+               "aka1_first_png.png", "aka2_first_png.png", "aka3_first_pic.png",'aka1_last_png.png',
+               "aka2_last_png.png", "aka3_last_pic.png"]
+
+def search_profile_fetch():
+    for i in pngs_to_get:
+        find_picture_location(i)
+
+def search_id_fetch():
+    findsearch = ["enter_id_box.png","search_press_box.png"]
+    for i in findsearch:
+        find_picture_location(i)
+        pyautogui.click(duration = .15)
+        pyautogui.hotkey('ctrl', 'v', interval=0.15)
+
+def move_to_word
+
+# search_profile_fetch()
+search_id_fetch()
--- pyautogui_onlytuples.py
+++ pyautogui_onlytuples.py
@@ -3,53 +3,82 @@ from pyautogui import moveTo
 from pyautogui import click
 from pyautogui import dragTo
 
-#FOR THIS TO WORK EACH BROWSER WINDOW HAS TO TAKE UP EXACTLY HALF OF THE SCREEN
+# FOR THIS TO WORK EACH BROWSER WINDOW HAS TO TAKE UP EXACTLY HALF OF THE SCREEN
 
-#todo: dyanamically create tuple coordinates for the djson marks so i later can just enter in a number and
+# todo: dyanamically create tuple coordinates for the djson marks so i later can just enter in a number and
 # it will copy that many for me
-#TODO:maybe make a function that will calculcate the next excel cell down on my template
+# TODO:maybe make a function that will calculcate the next excel cell down on my template
 
 wh = pyautogui.size()
 
-newtabtuple, searchidtuple, searchbuttontuple, lastnametuple, firstnametuple, midnametuple = (3184, 93), (3310, 378), (3554,376), (2936, 352), (3097, 345), (3243, 348)
-DOBholdbeginTuple, DOBholdendTuple, countyTuple = (3334,362), (3435,362), (3187, 584)
-testpaste = (2005,688)
-
+newtabtuple, searchidtuple, searchbuttontuple, lastnametuple, firstnametuple, midnametuple = (3184, 93), (3310, 378), (3554, 376), (2886, 352), (3036, 347), (3243, 348)
+DOBholdbeginTuple, DOBholdendTuple, countyTuple = (3334, 362), (3435, 362), (3144, 590)
+testpaste = (2005, 688)
+WI_court_dob = (1993, 534)
+last_name_box,first_name_box,county_box = (2099, 420), (2318,427),(2000,710)
 
 
 def tuplelist_func(ID_input):
     y = 264
     tuplelist = []
-    for i in range(1,ID_input + 1):
-        tuplevar = (2050,y)
+    for i in range(1, ID_input + 1):
+        tuplevar = (2050, y)
         tuplelist.append(tuplevar)
-        y +=20
+        y += 20
     return tuplelist
 
-def makeDjsonTuples(ID_input,tuplelist):
+
+def makeDjsonTuples(ID_input, tuplelist):
     djsonvarlist = []
-    for i in range(1,ID_input + 1):
+    for i in range(1, ID_input + 1):
         djsonvarname = "djsonTuple" + str(i)
         djsonvarlist.append(djsonvarname)
     excel_id_coordinates = dict(zip(djsonvarlist, tuplelist))
     return excel_id_coordinates
 
-def DOBcopyv():
-    moveTo(DOBholdbeginTuple)
-    dragTo(DOBholdendTuple,duration =.30)
-    pyautogui.hotkey('ctrl', 'c', interval = 0.15)
 
-def paste():
-    pyautogui.hotkey('ctrl', 'v', interval = 0.15)
+def DOBcopypaste(pastehere):
+    moveTo(DOBholdbeginTuple, duration=.15)
+    dragTo(DOBholdendTuple, duration=.30)
+    pyautogui.hotkey('ctrl', 'c', interval=0.15)pyautogui.hotkey(pyautogui.hotkey('ctrl', 'v', interval=0.15)'ctrl', 'v', interval=0.15)
+    moveTo(pastehere, duration=.15)
+    click(pastehere, duration=.15)
+    pyautogui.hotkey('ctrl', 'v', interval=0.15)
+
+
+def gen_copypaste(copyhere, pastethere):
+    moveTo(copyhere, duration=.15)
+    for i in range(0, 2):
+        click(copyhere, duration=.10)
+    moveTo(pastethere, duration=.15)
+    click(pastethere, duration=.15)
+    pyautogui.hotkey('ctrl', 'v', interval=0.15)
+
+def paste(pastehere):
+    pyautogui.hotkey('ctrl', 'v', interval=0.15)
+
+
+def get_L_name(lastnametuple, last_name_box):
+    gen_copypaste(lastnametuple, last_name_box)
+
+def get_F_name(firstnametuple, first_name_box):
+    gen_copypaste(firstnametuple, first_name_box)
+
+def get_county(countytuple, county_box):
+    gen_copypaste(countytuple,county_box)
 
 # newtabtuple, searchidtuple, searchbuttontuple, lastnametuple, firstnametuple, midnametuple = (3184, 93), (3310, 378), (3554,376), (2936, 352), (3097, 345), (3243, 348)
 # DOBholdbeginTuple, DOBholdendTuple, countyTuple = (3334,362), (3435,362), (3187, 584)
 # testpaste = (2005,688)
 
-ID_input = input("Number of ids:") or 20
+ID_input = 20 #input("Number of ids:") or 20
 tuplelist = tuplelist_func(ID_input)
 excel_id_coordinates = makeDjsonTuples(ID_input, tuplelist)
-
-
-
-
+# pastehere = testpaste
+pastehere1 = WI_court_dob
+DOBcopypaste(pastehere1)
+get_L_name(lastnametuple, last_name_box)
+get_F_name(firstnametuple,first_name_box)
+get_county(countyTuple,county_box)
+with open(r"C:\\Users\kschwartz\Desktop\DOB_pic.png","r+") as DOBpicture:
+    pyautogui.locateOnScreen(DOBpicture)
